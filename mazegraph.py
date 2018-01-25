a678d93f63dc7af442f48e6acf7f379f0096b96a
serramatutu <lucasvvop@hotmail.com>
2018-01-25 16:35:00 -0200

    Reimplemented graph with adjacency lists

--- mazegraph.py
+++ mazegraph.py
@@ -1,66 +1,62 @@
 import operator
 
-# _MazeGraphNode class
-class _MazeGraphNode:
+# MazeGraph class
+# implementa um grafo de pontos por lista de adjacência
+class MazeGraph:
     @staticmethod
     def _ispoint(p):
         return (isinstance(p, (tuple, list)) and 
                 isinstance(p[0], int) and isinstance(p[1], int))
     
-    def __init__(self, point, connections=None):
-        self.point = point;
-        
-        if connections != None:
-            if not isinstance(connections, list):
-                raise AttributeError("connections must be list of points")
-            for p in connections:
-                if not _MazeGraphNode._ispoint(p):
-                    raise AttributeError("connections must be list of points")
-            self._connections = connections
-        else:
-            self._connections = [];
-        
-    point = property(operator.attrgetter('_point'))
-    
-    @point.setter
-    def point(self, point):
-        if not _MazeGraphNode._ispoint(point):
-            raise AttributeError("point must be (int, int)")
-        self._point = point;
-    
-    def add_connection(self, point):
-        if point == None or not _MazeGraphNode._ispoint(point):
-            raise AttributeError("point must be (int, int)")
-        self._connections.append(point)
-        
-    def remove_connection(self, point):
-        if point in self._connections:
-            self._connections.remove(point)
-            
-    @property
-    def connection_count(self):
-        return len(self._connections)
-
-# MazeGraph class
-class MazeGraph:
     def __init__(self, start=None, end=None):
         self.start = start
         self.end = end
+        self._nodes = [] # lista de nós do grafo
         
+    def add_node(self, point)
+        if not point in self._nodes
+            self._nodes[point] = []
+            
+    def remove_node(self, point)
+        if point in self._nodes: # apenas se o nó existir
+            for edge in self._nodes[point]: # remove referências ao nó
+                self._nodes[edge].remove(point)
+            self._nodes.remove(point) # remove o nó em si
+            
+    def add_edge(self, a, b):
+        if not all(point in self._nodes for point in [a, b]):
+            raise ValueError("points a and b should be in graph already")
+        # conecta os dois na lista de adjacência
+        self._nodes[a].append(b);
+        self._nodes[b].append(a);
+        
+    def remove_edge(self, a, b):
+        if not all(point in self._nodes for point in [a, b]):
+            raise ValueError("points a and b should be in graph already")
+        # desconecta os dois na lista de adjacência
+        self._nodes[a].remove(b);
+        self._nodes[b].remove(a);
+    
+    def get_adjacent_edges(self, point)
+        return self._nodes[point];
+            
+            
     # Getters e setters
     
-    @property
-    def start(self):
-        return self._start.point
+    start = property(operator.attrgetter('_start'))
     
     @start.setter
     def start(self, point, connections=None):
-        self._start = _MazeGraphNode(point, connections)
+        if point != None and not MazeGraph._ispoint(point)
+            raise AttributeError("point should be (int, int)")
+        self.add_node(point)
+        self._start = point
     
-    @property
-    def end(self):
-        return self._end.point
+    end = property(operator.attrgetter('_end'))
     
     @end.setter
     def end(self, point, connections=None):
-        self._end = _MazeGraphNode(point, connections)
+        if point != None and not MazeGraph._ispoint(point)
+            raise AttributeError("point should be (int, int)")
+        self.add_node(point)
+        self._end = point
